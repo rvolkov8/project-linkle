8354930704838e119bfb821ba422b59da8bb4053
Rostyslav <volkov.rostyslav.8@gmail.com>
2023-08-03 12:51:31 +0300

    Implement Post mongoose model

---
+++ server/models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  body: { type: String, required: true },
+  picture: { type: String, default: null },
+  sharesPost: { type: Schema.Types.ObjectId, default: null },
+  comments: [{ type: Schema.Types.ObjectId, red: 'Comment' }],
+  likedBy: [{ type: Schema.Types.ObjectId, red: 'User' }],
+  sharedBy: [{ type: Schema.Types.ObjectId, red: 'User' }],
+  createdAt: { type: Date, default: Date.now() },
+});
+
+module.exports = mongoose.model('Post', postSchema);
