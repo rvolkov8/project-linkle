05c59526c530d985e3084bec9feee96e70a4134a
Rostyslav <volkov.rostyslav.8@gmail.com>
2023-08-07 20:48:37 +0300

    Enable CORS

--- server/app.js
+++ server/app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+var cors = require('cors');
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 // MongoDB connection
 require('./configs/mongodbConfig');
@@ -16,6 +22,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors(corsOptions));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', authRouter);
